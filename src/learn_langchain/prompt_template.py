f888852dca9dbf17c317aabfc64ad2de8aff1484
Prakash Mani <prakash.mani@rabobank.nl>
2025-06-21 10:59:25 +0530

    prompt template

--- src/learn_langchain/prompt_template.py
+++ src/learn_langchain/prompt_template.py
@@ -10,13 +10,13 @@ class PromptTemplate:
         model = init_chat_model(model="gemini-2.0-flash", model_provider="google_genai")
         prompt_template = ChatPromptTemplate.from_template(self.__prompt_template())
         prompt = "Hello, How are you?"
-        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Tamil"))
+        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Tamil", style="sarcastic"))
         info(response.content)
-        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Malayalam"))
+        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Tamil", style="formal"))
         info(response.content)
-        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Marathi"))
+        response = model.invoke(prompt_template.format_prompt(text=prompt, language="Tamil", style="informal"))
         info(response.content)
 
     @staticmethod
     def __prompt_template() -> str:
-        return "Translate the text that is delimited by triple backticks to {language}. I do not want the breakdown. text: ```{text}```"
+        return "Translate the text that is delimited by triple backticks to {language} in {style} way. I do not want the breakdown. text: ```{text}```"
